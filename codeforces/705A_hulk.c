d004252d9a547d74aa26485f9830e6ba0dadf6cc
Touhid Tamim <194215315+touhidtamim@users.noreply.github.com>
2026-07-17 22:05:40 +0600

    solved problem 705A

---
+++ codeforces/705A_hulk.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    for (int i = 1; i <= n; i++) {
+        if (i % 2 == 1) {
+            printf("I hate");
+        } else {
+            printf("I love");
+        }
+
+        if (i < n) {
+            printf(" that ");
+        } else {
+            printf(" it\n");
+        }
+    }
+
+    return 0;
+}
