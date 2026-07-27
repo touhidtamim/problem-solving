85578d12fee82001fc7862d4ca593e6bc4114830
Touhid Tamim <194215315+touhidtamim@users.noreply.github.com>
2026-07-27 21:39:07 +0600

    solved problem 71A

---
+++ codeforces/70A_way_too_long_words.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+    char word[101];
+    int length, i;
+
+    for (i = 0; i < n; i++) {
+        scanf("%s", word);
+        length = strlen(word);
+
+        if (length > 10) {
+            printf("%c%d%c\n", word[0], length - 2, word[length - 1]);
+        } else {
+            printf("%s\n", word);
+        }
+    }
+
+    return 0;
+}
