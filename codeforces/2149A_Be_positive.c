60346fae463404576c4f44e153fb709e0b9c03b7
Touhid Tamim <194215315+touhidtamim@users.noreply.github.com>
2026-08-05 23:07:20 +0600

    solution updated- 2149A

--- codeforces/2149A_Be_positive.c
+++ codeforces/2149A_Be_positive.c
@@ -3,22 +3,26 @@
 int main() {
     int t;
     scanf("%d", &t);
-    while (t--) {
-        int n, zeros = 0, neg_ones = 0;
+
+    for (int i = 0; i < t; i++) {
+        int n;
         scanf("%d", &n);
-        for (int i = 0; i < n; i++) {
+
+        int neg = 0, zero = 0;
+        for (int j = 0; j < n; j++) {
             int x;
             scanf("%d", &x);
-            if (x == 0) zeros++;
-            if (x == -1) neg_ones++;
+            if (x == -1) neg++;
+            if (x == 0) zero++;
         }
 
-        int ops = zeros;
-        if ((neg_ones % 2) != 0) {
-            ops++;
+        int ops = zero;
+        if (neg % 2 != 0) {
+            ops += 2;
         }
-        
+
         printf("%d\n", ops);
     }
+
     return 0;
 }
