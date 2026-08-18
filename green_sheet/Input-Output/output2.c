cd33023120b4698c61bf55f52f11c6604c4fb717
Touhid Tamim <194215315+touhidtamim@users.noreply.github.com>
2026-08-18 17:21:59 +0600

    solved output 2 from beecrowd

---
+++ green_sheet/Input-Output/output2.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+
+    for (int i = 1; i < 40; i++) {
+        printf("-");
+    }
+    printf("\n");
+
+    printf("|        Roberto                      |\n");
+    printf("|                                     |\n");
+    printf("|        5786                         |\n");
+    printf("|                                     |\n");
+    printf("|        UNIFEI                       |\n");
+
+    for (int i = 1; i < 40; i++) {
+        printf("-");
+    }
+    printf("\n");
+
+    return 0;
+}
