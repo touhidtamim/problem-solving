723043ed5d12d72bd0792f65cb617ac6a0ccd921
Touhid Tamim <194215315+touhidtamim@users.noreply.github.com>
2026-08-24 23:23:07 +0600

    solved problem no 1759

---
+++ green_sheet/Loop/HoHoHo.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int N;
+    scanf("%d", &N);
+
+    for (int i = 1; i <= N; i++) {
+        if (i == N) {
+            printf("Ho!\n");
+        } else {
+            printf("Ho ");
+        }
+    }
+
+    return 0;
+}
